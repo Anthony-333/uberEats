04397ef797f7917581939c1e0be7314acfb31d8a
tuneng333 <anthonyoling333@gmail.com>
2022-07-06 18:45:27 +0800

    expo modules added

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -16,16 +17,36 @@ const App = () => {
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg',
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.name}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style={styles.name}>$1.99</Text>
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  restaurantContainer: {
+    width: '100%',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 5 / 4,
+  },
+});
 
 export default App;
